3b9f51f567f8fb6995a241d40332e8aa87fb4ee8
josdorrod <josediegodorado@hotmail.com>
2024-06-28 22:48:40 +0200

    Fixing display.c #10
    Adjusted gap width and dash width

--- code/display.c
+++ code/display.c
@@ -3,13 +3,6 @@
 // Function to display the 2D array in a grid format
 void display(int **array, int length)
 {
-    // Print horizontal line
-    printf("\n");
-    for(int i=0; i<length; i++)
-        printf("----");
-    printf("\n");
-
-
     // Calculate the width required for displaying numbers
     int num_width = 1;
     while(num_width < length*length)
@@ -17,22 +10,30 @@ void display(int **array, int length)
     num_width = (int)log10(num_width);
 
 
+    // Print horizontal line
+    int width = length * (3 + num_width) + 1;
+    printf("\n");
+    for(int i=0; i<width; i++)
+        printf("-");
+    printf("\n");
+
+
     // Print array elements in a grid
     for(int i=0; i<length; i++)
     {
         for(int j=0; j<length; j++)
         {
-            if(array[i][j] != length*length)
-                printf("| %*d ",num_width,array[i][j]);   // %* the width of the filed will be taken as an arument we could have also specified big constants too
-            else
-                printf("|    ");                            // Placeholder for empty cell
+            if(array[i][j] !=length*length)
+                printf("| %*d ", num_width, array[i][j]);   // %* allow to specify a custom width (num_width) 
+            else                                            // we could have also specified big constants too
+                printf("| %*s ", num_width, " ");           // Placeholder for empty cell
         }
         printf("|\n");
     }
 
 
     // Print horizontal line
-    for(int i=0; i<length; i++)
-        printf("----");
+    for(int i=0; i<width; i++)
+        printf("-");
     printf("\n");
 }
